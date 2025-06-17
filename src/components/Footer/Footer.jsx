c30b041339f400958fe4cf3c584dd9a01b908cc5
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-06-17 14:58:58 -0300

    feat: adiciona componente Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import "./Footer.css";
+import { useCallback } from "react";
+
+const Footer = () => {
+  function getCurrentYear() {
+    const today = new Date();
+    const year = today.getFullYear();
+    return year;
+  }
+
+  return (
+    <footer className="footer">
+      <p className="footer__copyrights">
+        &copy; {getCurrentYear()} João Luiz Cambraia
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
