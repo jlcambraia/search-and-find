9392002a7cc6f5115759a5f7fdc6e5990804b22e
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-06-17 15:00:48 -0300

    feat: adiciona config para objetos

---
+++ src/config/objectsConfig.js
@@ -0,0 +1,62 @@
+export const objectConfig = [
+  {
+    id: 1,
+    top: "50%",
+    left: "20%",
+    content: "object1",
+  },
+  {
+    id: 2,
+    top: "55%",
+    left: "60%",
+    content: "object2",
+  },
+  {
+    id: 3,
+    top: "35%",
+    left: "90%",
+    content: "object3",
+  },
+  {
+    id: 4,
+    top: "20%",
+    left: "65%",
+    content: "object4",
+  },
+  {
+    id: 5,
+    top: "35%",
+    left: "40%",
+    content: "object5",
+  },
+  {
+    id: 6,
+    top: "17%",
+    left: "80%",
+    content: "object6",
+  },
+  {
+    id: 7,
+    top: "30%",
+    left: "55%",
+    content: "object7",
+  },
+  {
+    id: 8,
+    top: "85%",
+    left: "80%",
+    content: "object8",
+  },
+  {
+    id: 9,
+    top: "15%",
+    left: "10%",
+    content: "object9",
+  },
+  {
+    id: 10,
+    top: "85%",
+    left: "20%",
+    content: "object10",
+  },
+];
