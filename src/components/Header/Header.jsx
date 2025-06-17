6ff438f3404d8dcdea18025227b47ce45aa49d34
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-06-17 14:59:08 -0300

    feat: adiciona componente Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <h1 className="header__title">Procure e Ache</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
